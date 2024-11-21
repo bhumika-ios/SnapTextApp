adeab582498a49c1800b749b03eb2ca9db43ed55
bhumika-ios <bhumika.iosdeveloper.com>
2024-11-21 11:07:15 +0530

    Initial Commit

---
+++ SnapTextApp/SnapTextAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnapTextAppApp.swift
+//  SnapTextApp
+//
+//  Created by Bhumika Patel on 21/11/24.
+//
+
+import SwiftUI
+
+@main
+struct SnapTextAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SnapTextAppTests/SnapTextAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  SnapTextAppTests.swift
+//  SnapTextAppTests
+//
+//  Created by Bhumika Patel on 21/11/24.
+//
+
+import Testing
+@testable import SnapTextApp
+
+struct SnapTextAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
